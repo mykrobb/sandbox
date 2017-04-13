1677c1de8457ffedba373c5e296c21974b075e79
mykrobb <mykrobb@gmail.com>
2017-04-13 10:28:13 -0400

    third comment from remote

--- junk.java
+++ junk.java
@@ -2,6 +2,7 @@ public class Junk {
 	public static void main(String[] args) {
 		//new comment from remote
 		//second comment from remote
+		//third comment from remote
 		System.out.println("Hello world");
 		//comment from local
 		//second comment from local
